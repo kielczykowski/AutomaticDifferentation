c20a95fa245b674a4b46b1a1b6c75001230b88fd
mki <michal.k.kielczykowski@gmail.com>
2020-06-14 15:23:43 +0200

    basic functions forward/reverse derivatives

--- compareJuliaDiff.jl
+++ compareJuliaDiff.jl
@@ -2,6 +2,7 @@ using Pkg, BenchmarkTools, Plots
 Pkg.add("ForwardDiff")
 Pkg.add("ReverseDiff")
 using ForwardDiff
+using ReverseDiff
 
 import Base: sin, cos, tan
 
@@ -11,17 +12,41 @@ import Base: sin, cos, tan
 
 ReLu(x) = x > zero(x) ? x : zero(x)
 
+Rosenbrock(x, y) = (1.0 - x*x) + 100.0*(y - x*x)*(y - x*x)
+
 # f(x::Vector) = sum(sin, x) + prod(tan, x) * sum(sqrt, x);
+dRosenbrockdx = (x, y) -> ForwardDiff.derivative(Rosenbrock, x, y)
 
 
-function fDiff()
+
+function forwardModeDerivative()
+    x = y = collect(-π:0.05:+π)
+
+    # y = x -> ForwardDiff.derivative(ReLu, x)
+    # display((y.(x)))
+    p1 = plot(x, ForwardDiff.derivative.(sin, x), title = "Sin derivative")
+    p2 = plot(x, ForwardDiff.derivative.(cos, x), title = "Cos derivative")
+    p3 = plot(x, ForwardDiff.derivative.(tan, x), title = "Tan derivative")
+    p4 = plot(x, ForwardDiff.derivative.(ReLu, x), title = "ReLu derivative")
+    plot(p1, p2, p3, p4, layout = (2, 2), legend = false)
+    # plot(x, dRosenbrockdx.(x, y))
+
+end
+
+function reverseModeDerivative()
+    rsin(a) = sum(sin.(a))
+    rcos(a) = sum(cos.(a))
+    rtan(a) = sum(tan.(a))
+    rReLu(a) = sum(ReLu.(a))
+
     x = collect(-π:0.05:+π)
 
-    y = x -> ForwardDiff.derivative(ReLu, x)
+    p1 = plot(x, ReverseDiff.gradient(rsin, x), title = "Sin Derivative")
+    p2 = plot(x, ReverseDiff.gradient(rcos, x), title = "Cos Derivative")
+    p3 = plot(x, ReverseDiff.gradient(rtan, x), title = "Tan Derivative")
+    p4 = plot(x, ReverseDiff.gradient(rReLu, x), title = "ReLu Derivative")
+    plot(p1, p2, p3, p4, layout = (2, 2), legend = false)
 
-    y.(x)
-    display((y.(x)))
-    plot(x, y.(x))
 end
 
 
@@ -42,7 +67,8 @@ end
 
 
 function main()
-    fDiff()
+    # forwardModeDerivative()
+    reverseModeDerivative()
     # fDiffJacobian()
 end
 
